9daef1a23b1f2eca216834a8976167a2bcbaaa5e
Keison Roney <110565730+keisonroney@users.noreply.github.com>
2023-02-14 13:55:52 -0300

    Initial commit

---
+++ EXERCÍCIOS/2-sintaxe-e-comentários/ex02.py
@@ -0,0 +1,15 @@
+#Exercício de sintaxe e criação de comentários em Python
+
+dia="Terça Feira" #A quebra de linha indica a finalização de comandos. 
+mes= " 14 de Fevereiro";print(dia + " - " + mes)
+#ponto e vírgula é usado para separar comandos na mesma linha. 
+
+""" 
+Três aspas criam um comentário de multiplas linhas. 
+"""
+
+if 10 > 2: 
+    print("Maior") #o recuo dentro de um if determina o escopo do if.  
+    print("Teste 1")
+
+print("FIM")
